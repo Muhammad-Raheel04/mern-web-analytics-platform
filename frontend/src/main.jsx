a18f46a712a435b4d96e6c74bc1fb9e336eb7638
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-07 22:11:48 +0500

    feat(main): setup Redux Provider and Redux Store in main entry file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
       <App />
-      <Toaster 
-      position='bottom-right' 
-      reverseOrder={false}
-      toastOptions={{
-        style:{
-          background:"#042f2b",
-          color:"#05fce8",
-          padding:"12px 16px",
-          borderRadius:"10px",
-        },
-        success:{
-          style:{
-            background:"#042f2b",
-          }
-        },
-        error:{
-          style:{
-            background:"#042f2b",
-          }
-        },
-        duration:3000,
-      }}
+      <Toaster
+        position='bottom-right'
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            background: "#042f2b",
+            color: "#05fce8",
+            padding: "12px 16px",
+            borderRadius: "10px",
+          },
+          success: {
+            style: {
+              background: "#042f2b",
+            }
+          },
+          error: {
+            style: {
+              background: "#042f2b",
+            }
+          },
+          duration: 3000,
+        }}
       ></Toaster>
+    </Provider>
+
   </StrictMode>,
 )
