3114a34b3fb7113c579850ac30363fe558b1c3b3
Muhammad Raheel <raheel31102004@gmail.com>
2026-06-14 22:07:48 +0500

    feat: integrate user based limiter at /:siteId get endpoint

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getAnalytics, getVersion, serveLoader, serveTrackerScript, trackPageVisits } from '../controllers/analyticsController.js';
 import { isAuthenticated } from '../middleware/isAuthenticated.js';
+import { analyticsLimiter } from '../middleware/analyticsLimiter.js';
 const router=express.Router();
 
 router.get('/version',getVersion);
 router.get('/analytics.js',serveLoader);
 router.get('/analytics.v:version.js',serveTrackerScript);
-router.get('/:siteId',isAuthenticated,getAnalytics)
+router.get('/:siteId',isAuthenticated,analyticsLimiter,getAnalytics)
 router.post('/track',trackPageVisits);
 
 export default router;
